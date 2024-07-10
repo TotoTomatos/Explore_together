ef381eedebed5e0d44c973edf3b3ae6bcbb17249
Xu <1440930392@qq.com>
2024-07-10 17:11:50 +0800

    feat(权限验证): 用户登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -36,5 +38,35 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //TODO 发送验证码
         session.setAttribute("code",random);
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号码格式不正确");
+        }
+        String cacheCode = session.getAttribute("code").toString();
+        if (cacheCode == null || !cacheCode.equals(loginForm.getCode())){
+            return Result.fail("验证码错误");
+        }
+
+        User user = super.query().eq("phone", phone).one();
+        if(user == null){
+            user = createUserWithPhone(phone);
+        }
+
+        session.setAttribute("user",user);
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX+RandomUtil.randomString(10));
+        //保存用户
+        save(user);
+        return user;
     }
 }
